122c9f64e9bbede62be412426aadeb1b203e0456
Shyam <107743761+Maxxtin@users.noreply.github.com>
2025-05-01 14:43:03 +0000

    add

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,53 +5,6 @@ jQuery(document).ready(function ($) {
         $(".loaded").fadeOut();
         $(".preloader").delay(1000).fadeOut("slow");
     });
-    /*---------------------------------------------*
-     * Mobile menu
-     ---------------------------------------------*/
-    $('#bs-example-navbar-collapse-1').find('a[href*=#]:not([href=#])').click(function () {
-        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-            if (target.length) {
-                $('html,body').animate({
-                    scrollTop: (target.offset().top - 0)
-                }, 1000);
-                if ($('.navbar-toggle').css('display') != 'none') {
-                    $(this).parents('.container').find(".navbar-toggle").trigger("click");
-                }
-                return false;
-            }
-        }
-    });
-
-
-// slick slider active 
-    $(".main_home_slider").slick({
-        dots: false,
-        infinite: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        prevArrow:"<i class='fa fa-angle-left nextprevleft'></i>",
-        nextArrow:"<i class='fa fa-angle-right nextprevright'></i>"
-    });
-
-//    $(".study_slider").slick({
-//        dots: true,
-//        slidesToShow: 1,
-//        slidesToScroll: 1
-//    });
-    $(".study_slider").slick({
-        dots: true,
-        arrows:false,
-        slidesToShow: 1,
-        slidesToScroll: 1
-    });
-
-    /*---------------------------------------------*
-     * STICKY scroll
-     ---------------------------------------------*/
-
-    $("").localScroll();
 
     /*---------------------------------------------*
      * WOW
@@ -62,9 +15,7 @@ jQuery(document).ready(function ($) {
     });
     wow.init();
 
-
-// magnificPopup
-
+    // magnificPopup
     $('.portfolio-img').magnificPopup({
         type: 'image',
         gallery: {
@@ -72,24 +23,19 @@ jQuery(document).ready(function ($) {
         }
     });
 
-
-
-
-//---------------------------------------------
-// Counter 
-//---------------------------------------------
+    //---------------------------------------------
+    // Counter 
+    //---------------------------------------------
 
     $('.statistic-counter').counterUp({
         delay: 10,
         time: 2000
     });
 
-// main-menu-scroll
-
+    // main-menu-scroll
     jQuery(window).scroll(function () {
         var top = jQuery(document).scrollTop();
         var height = 300;
-        //alert(batas);
 
         if (top > height) {
             jQuery('.navbar-fixed-top').addClass('menu-scroll');
@@ -98,23 +44,7 @@ jQuery(document).ready(function ($) {
         }
     });
 
-// scroll Up
-
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 600) {
-            $('.scrollup').fadeIn('slow');
-        } else {
-            $('.scrollup').fadeOut('slow');
-        }
-    });
-
-    $('.scrollup').click(function () {
-        $("html, body").animate({scrollTop: 0}, 1000);
-        return false;
-    });
-
-
-// scrool Down
+    // scrool Down
     $('.scrooldown a').bind('click', function () {
         $('html , body').stop().animate({
             scrollTop: $($(this).attr('href')).offset().top - 160
@@ -122,32 +52,25 @@ jQuery(document).ready(function ($) {
         event.preventDefault();
     });
 
-
-
-// Portfoliowork init
+    // Portfoliowork init
     jQuery('#portfoliowork').mixItUp({
         selectors: {
             target: '.tile',
             filter: '.filter'
-                    //           sort: '.sort-btn'
         },
         animation: {
             animateResizeContainer: false,
             effects: 'fade scale'
         }
-
     });
 
-// dropdown menu
+    // dropdown menu
     $('.dropdown-menu').click(function (e) {
         e.stopPropagation();
     });
 
     //End
-
 });
-
-
 
 $(document).on("scroll", function () {
     if ($(document).scrollTop() > 120) {
